1482e544a4d05dbfdac649baaca28002794411c4
Clément Mauperon <47110246+tensaiji@users.noreply.github.com>
2022-02-08 12:32:32 -0500

    tests fonctions de manipulation de string des messages

--- protocole/test/src/message/Message.cpp
+++ protocole/test/src/message/Message.cpp
@@ -22,7 +22,7 @@ namespace BTTP
                 {
                 }
 
-                const bool Message::decoupage_correct(
+                void Message::decoupage_correct(
                     const std::string paquet, const std::string separateur,
                     const std::string elements[], const size_t n_elements
                 )
@@ -31,9 +31,8 @@ namespace BTTP
                     EXPECT_TRUE(resultat.size() == n_elements);
                     for (int p = 0; p < n_elements; p++)
                         EXPECT_TRUE(resultat[p] == elements[p]);
-                    return true;
                 }
-                const bool Message::decoupage_correct(
+                void Message::decoupage_correct(
                     const std::string paquet, const char separateur,
                     const std::string elements[], const size_t n_elements
                 )
@@ -42,34 +41,72 @@ namespace BTTP
                     EXPECT_TRUE(resultat.size() == n_elements);
                     for (int p = 0; p < n_elements; p++)
                         EXPECT_TRUE(resultat[p] == elements[p]);
-                    return true;
+                }
+
+                void Message::jointure_correcte(
+                    const std::string attendu, const std::string separateur,
+                    const std::string elements[], const size_t n_elements
+                )
+                {
+                    std::vector<std::string> v;
+                    for (size_t e = 0; e < n_elements; e++) v.push_back(elements[e]);
+                    const std::string jointure = Protocole::Messages::joindre(v, separateur);
+                    EXPECT_EQ(jointure, attendu);
+                }
+
+                void Message::jointure_correcte(
+                    const std::string attendu, const char separateur,
+                    const std::string elements[], const size_t n_elements
+                )
+                {
+                    std::vector<std::string> v;
+                    for (size_t e = 0; e < n_elements; e++) v.push_back(elements[e]);
+                    const std::string jointure = Protocole::Messages::joindre(v, separateur);
+                    EXPECT_EQ(jointure, attendu);
                 }
 
                 // UTILITAIRES
                 TEST(Message, DecoupagePaquet)
                 {
-                    EXPECT_TRUE(Message::decoupage_correct("entete-contenu-pied", '-', Message::elements_x3, 3));
-                    EXPECT_TRUE(Message::decoupage_correct("entete°sep°contenu°sep°pied", "°sep°", Message::elements_x3, 3));
-                    EXPECT_TRUE(Message::decoupage_correct(
+                    Message::decoupage_correct("entete-contenu-pied", '-', Message::elements_x3, 3);
+                    Message::decoupage_correct("entete°sep°contenu°sep°pied", "°sep°", Message::elements_x3, 3);
+                    Message::decoupage_correct(
                         "sara@luca@clément p.@robin@clément m.@hemmbouk",
                         '@', Message::elements_x6, 6
-                    ));
-                    EXPECT_TRUE(Message::decoupage_correct(
+                    );
+                    Message::decoupage_correct(
                         "saraøkaylucaøkayclément p.økayrobinøkayclément m.økayhemmbouk",
                         "økay", Message::elements_x6, 6
-                    ));
-                    EXPECT_TRUE(Message::decoupage_correct(
+                    );
+                    Message::decoupage_correct(
                         "un\\deux\\trois\\quatre\\cinq\\six\\sept\\huit\\neuf\\dix\\onze\\douze",
                         '\\', Message::elements_x12, 12
-                    ));
-                    EXPECT_TRUE(Message::decoupage_correct(
+                    );
+                    Message::decoupage_correct(
                         "un£$£$deux£$£$trois£$£$quatre£$£$cinq£$£$six£$£$sept£$£$huit£$£$neuf£$£$dix£$£$onze£$£$douze",
                         "£$£$", Message::elements_x12, 12
-                    ));
+                    );
                 }
                 TEST(Message, JointurePaquet)
                 {
-                    
+                    Message::jointure_correcte("entete-contenu-pied", '-', Message::elements_x3, 3);
+                    Message::jointure_correcte("entete°sep°contenu°sep°pied", "°sep°", Message::elements_x3, 3);
+                    Message::jointure_correcte(
+                        "sara@luca@clément p.@robin@clément m.@hemmbouk",
+                        '@', Message::elements_x6, 6
+                    );
+                    Message::jointure_correcte(
+                        "saraøkaylucaøkayclément p.økayrobinøkayclément m.økayhemmbouk",
+                        "økay", Message::elements_x6, 6
+                    );
+                    Message::jointure_correcte(
+                        "un\\deux\\trois\\quatre\\cinq\\six\\sept\\huit\\neuf\\dix\\onze\\douze",
+                        '\\', Message::elements_x12, 12
+                    );
+                    Message::jointure_correcte(
+                        "un£$£$deux£$£$trois£$£$quatre£$£$cinq£$£$six£$£$sept£$£$huit£$£$neuf£$£$dix£$£$onze£$£$douze",
+                        "£$£$", Message::elements_x12, 12
+                    );
                 }
             }
         }

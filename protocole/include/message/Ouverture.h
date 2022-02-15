09695c089c5fd8c2ed3a1be96108ead4a07d1e94
Clément Mauperon <47110246+tensaiji@users.noreply.github.com>
2022-02-15 09:16:15 -0500

    luda lets gooooo

---
+++ protocole/include/message/Ouverture.h
@@ -0,0 +1,20 @@
+#ifndef H_BTTP_MESSAGE_OUVERTURE
+#define H_BTTP_MESSAGE_OUVERTURE
+
+#include "Message.h"
+
+namespace BTTP
+{
+    namespace Protocole
+    {
+        namespace Messages
+        {
+            class Ouverture : public Message
+            {
+            private:
+            };
+        }
+    }
+}
+
+#endif

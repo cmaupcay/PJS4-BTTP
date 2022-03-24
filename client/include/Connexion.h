c6e1a5a12b48c8aac45b7ce9242569b8643db484
likir09 <luca.randazzo@protonmail.com>
2022-03-24 11:33:50 +0000

    implémentation interface IConnexion

--- client/include/Connexion.h
+++ client/include/Connexion.h
@@ -2,6 +2,8 @@
 #define BTTP_CLIENT_CONNEXION
 
 #include "Racine.h"
+#include "serveurs/Serveur.h"
+#include <chrono>
 #include <asio.hpp>
 
 namespace BTTP
@@ -11,7 +13,21 @@ namespace BTTP
         class Connexion : public Protocole::IConnexion
         {
             // TODO Implémentation de l'interface définie dans le protocole grâce à asio.
-        }
+
+            private:
+                asio::ip::tcp::socket* _socket;
+                const Serveur& _serveur;
+                asio::error_code _erreur;
+
+            public:
+                Connexion(const Serveur& serveur);
+                const bool ouverte() override;
+                void ouvrir() override;
+                inline void fermer() override {this->_socket->close();}
+                void envoyer(const std::string message_prepare) override;
+                const std::string recevoir() override;
+
+        };
     }
 }
 
---
+++ client/src/Connexion.cpp
@@ -0,0 +1,68 @@
+#include "../include/Connexion.h"
+
+namespace BTTP 
+{
+
+    namespace Client
+    {
+
+        Connexion::Connexion(const Serveur& serveur) : _serveur { serveur }
+        {
+            asio::io_context contexte;
+            this->_socket = new asio::ip::tcp::socket(contexte);
+
+        }
+
+        void Connexion::ouvrir() 
+        {
+
+            asio::ip::tcp::endpoint endpoint(asio::ip::make_address(_serveur.adresse(), _erreur), _serveur.port());
+            this->_socket->connect(endpoint, _erreur); 
+
+        }
+
+        void Connexion::envoyer(const std::string message_prepare) 
+        {
+            if(this->_socket->is_open()) 
+                this->_socket->write_some(asio::buffer(message_prepare.data(), message_prepare.size()), _erreur);
+
+            //TODO Gestion erreur socket fermé
+
+        }
+
+        const std::string Connexion::recevoir() 
+        {
+            std::string message = "";
+
+            size_t taille;
+
+            const std::chrono::time_point<std::chrono::system_clock> debut = std::chrono::system_clock::now();
+            std::chrono::time_point<std::chrono::system_clock> fin;
+            const double tempsMax = (double) BTTP_TIMEOUT;
+
+            while((taille = this->_socket->available()) == 0) 
+            {
+
+                fin = std::chrono::system_clock::now();
+                
+                auto temps = std::chrono::duration_cast<std::chrono::milliseconds>(fin - debut);
+
+                if(temps.count() >= tempsMax)
+                    return ""; //TODO Exception timeout
+
+            }
+
+            std::vector<char> buffer(taille);
+
+            this->_socket->read_some(asio::buffer(buffer.data(), buffer.size()), _erreur);
+
+            for(auto c : buffer)
+                message += c;
+
+            return message;
+
+        }
+
+    }
+
+}

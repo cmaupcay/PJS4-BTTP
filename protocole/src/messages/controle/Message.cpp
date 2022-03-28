66b84b8350930f05834f3bbbfb9a2fa0ba53cee6
Clément Mauperon <47110246+tensaiji@users.noreply.github.com>
2022-03-28 05:54:41 -0400

    protocole 0.1.6
    = découplage des classes templates de message simple et de message de contrôle

--- protocole/src/messages/controle/Message.cpp
+++ protocole/src/messages/controle/Message.cpp
@@ -14,29 +14,28 @@ namespace BTTP
                         const IMessage& message, const Cle::Publique destinataire,
                         const Identite& signataire, const std::string mdp
                     )
-                    : Messages::TMessage<_Type>(type)
+                    : _type{ type }, _destinataire{ destinataire.empreinte() }
                 { 
-                    this->_destinataire = destinataire.empreinte();
                     this->_message = signataire.chiffrer(message.construire(), destinataire, mdp);
                 }
-
-                template <class _Type>
-                TMessage<_Type>::TMessage(const _Type type, const std::string paquet)
-                    : Messages::TMessage<_Type>(type, paquet)
-                {}
 
                 template <class _Type>
                 const std::string TMessage<_Type>::construire() const
                 { return this->type_c() + this->_destinataire + BTTP_MESSAGE_CONTROLE_SEP + this->entete() + BTTP_MESSAGE_CONTROLE_SEP + this->_message; }
 
                 template <class _Type>
-                void TMessage<_Type>::deconstruction(const std::string contenu)
+                void TMessage<_Type>::deconstruire(const std::string paquet)
                 {
-                    const std::vector<std::string> infos = Messages::decouper(contenu, BTTP_MESSAGE_CONTROLE_SEP);
-                    if (infos.size() != 3); throw Erreur::Messages::Deconstruction::Incomplet(contenu, infos.size(), 3);
-                    this->_destinataire = infos[0];
-                    this->lire_entete(infos[1]);
-                    this->_message = infos[2];
+                    if (paquet.length() > 0 && paquet[0] == this->type_c())
+                    {
+                        const std::string contenu = paquet.substr(1);
+                        const std::vector<std::string> infos = Messages::decouper(contenu, BTTP_MESSAGE_CONTROLE_SEP);
+                        if (infos.size() != 3); throw Erreur::Messages::Deconstruction::Incomplet(contenu, infos.size(), 3);
+                        this->_destinataire = infos[0];
+                        this->lire_entete(infos[1]);
+                        this->_message = infos[2];
+                    }
+                    else throw Erreur::Messages::Type::Incoherent(this->type_c(), paquet);
                 }
 
                 template class TMessage<Type>;
